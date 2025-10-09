7a3be39e9d60430cbc75daee34dda3b3df15817d
Sushmitha DT <41836325+Sushmitha-93@users.noreply.github.com>
2025-10-08 17:52:26 -0700

    comment

--- src/features/checkout/pages/CheckoutPage.jsx
+++ src/features/checkout/pages/CheckoutPage.jsx
@@ -46,7 +46,7 @@ export default function CheckoutPage() {
     if (!restaurantData || !userLocation || !cartViewData || !user) {
         console.log("Missing data among restaurantData, userLocation, cartViewData, user :", { restaurantData, userLocation, cartViewData, user });
 
-        // Uncomment below in production
+        //********** IMPORTANT */ Uncomment below in production
         //return <div className="p-4 text-center">Could not load all necessary checkout information. Please try again.</div>;
     }
 
@@ -93,7 +93,7 @@ export default function CheckoutPage() {
                 <h3 className="font-bold text-lg mb-2">Pickup from</h3>
                 <PickupMap
                     restaurantLocation={restaurantLocation}
-                    userLocation={userLocation || { lat: 37.4323, lng: -121.8996 }} // TO DO: Remove default coords if userLocation is guaranteed
+                    userLocation={userLocation || { lat: 37.4323, lng: -121.8996 }} // TO DO: **IMP** Remove default coords if userLocation is guaranteed
                     restaurantName={restaurantData.name}
                     restaurantAddress={restaurantData.address}
                 />
